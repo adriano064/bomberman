27d075bd2c1ae8427dcae5bdbb3b5baf0c04b5c9
Pedro Ferreira Monteiro Filho <20211170150004@ifrn.local>
2023-11-30 13:19:02 -0300

    Segundo Player - Finalizado

--- att de wen/Assets/Script/movimento.cs
+++ att de wen/Assets/Script/movimento.cs
@@ -102,5 +102,6 @@ public class movimento : MonoBehaviour
     private void OnDeathSequenceEnded()
     {
         gameObject.SetActive(false);
+        FindObjectOfType<GameManager>().CheckWinState();
     }
 }
